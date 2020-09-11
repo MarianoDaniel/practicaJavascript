1dfb785217522aa4fc25fb36f9495f0019b5185a
Mariano <mavico@ignetworks.com>
2020-09-11 12:28:26 -0300

    practica

--- async-await.js
+++ async-await.js
@@ -4,16 +4,16 @@ const fabricaDePromesas = (indice) => new Promise((resolve, reject) => {
     console.log('tiempoRejected', tiempoRejected);
     console.log('tiempoResolver', tiempoResolver);
 
-    setTimeout(() => {
-        reject(`la promesa con índice ${indice} falló`)
-    }, tiempoRejected)
+      setTimeout(() => {
+          reject(`la promesa con índice ${indice} falló`)
+      }, tiempoRejected) 
 
-    setTimeout(() => {
+     setTimeout(() => {
         resolve(`promesa con índice ${indice} satisfecha`)
-    }, tiempoResolver)
+    }, tiempoResolver) 
 });
 
-let misPromesas = [];
+/* let misPromesas = [];
 for (let i = 0; i < 10; i++) {
     //spread Operator para agregarle la nueva promesa a las que ya hay
     misPromesas = [...misPromesas, fabricaDePromesas(i)];
@@ -23,10 +23,19 @@ misPromesas.forEach(promesa =>
         .then(
         respuesta => console.log(respuesta),
         razon => console.log(razon))
-)
-        
+) */
+
 
 /* miPromesa.then(
     respuesta => console.log(respuesta),
     razon => console.log(razon)); */
-
+//Al anteponer async estás diciendo que la función te va a devolver una promesa
+async function miAsyncFunction() {
+    try {
+        const miPromesa1 = await fabricaDePromesas(1);
+    console.log('este es el valor de miPromesa1 ', { miPromesa1 });
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
