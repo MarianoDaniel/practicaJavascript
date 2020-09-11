b69429207f343d5372d79078f4c1570cc94d5422
Mariano <mavico@ignetworks.com>
2020-09-11 09:52:40 -0300

    asyncawait.js agregado

--- async-await.js
+++ async-await.js
@@ -1,16 +1,3 @@
-/* console.log('Hola')
-
-function MiObjeto(nombre, apellido) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.getNombreCompleto = function () {
-        return `${this.nombre} ${this.apellido}`;
-    }
-}
-MiObjeto.prototype */
-
-
-
 const fabricaDePromesas = (indice) => new Promise((resolve, reject) => {
     const tiempoRejected = Math.floor(Math.random() * 10000) + 1000;
     const tiempoResolver = Math.floor(Math.random() * 10000) + 1000;
@@ -31,11 +18,14 @@ for (let i = 0; i < 10; i++) {
     //spread Operator para agregarle la nueva promesa a las que ya hay
     misPromesas = [...misPromesas, fabricaDePromesas(i)];
 }
+misPromesas.forEach(promesa =>
+    promesa
+        .then(
+        respuesta => console.log(respuesta),
+        razon => console.log(razon))
+)
+        
 
-
-Promise.allSettled(misPromesas).then(
-    respuesta => console.log(respuesta),
-    razon => console.log(razon))
 /* miPromesa.then(
     respuesta => console.log(respuesta),
     razon => console.log(razon)); */
