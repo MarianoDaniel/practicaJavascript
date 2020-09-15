e103d0a63930985493989fc8f2e22acb2d985772
Mariano <mavico@ignetworks.com>
2020-09-15 18:54:22 -0300

    prácticas con fetch

--- ejemplo-fetch.js
+++ ejemplo-fetch.js
@@ -1,1 +1,48 @@
-console.log('Ejemplo Fech');
+//Document es la API que el navegador me da para jugar con el html de la página
+const listaUsuarios = document.getElementById('lista-usuarios');
+const boton = document.getElementById('boton');
+let usuarios = [];
+
+function reqListener() {
+    const usuarios = JSON.parse(this.responseText);
+    console.log(usuarios)
+    const usuariosRender = usuarios.map(usuario => `<li>${usuario.nombre}</li>`).join("");
+    console.log(usuariosRender);
+    listaUsuarios.innerHTML = usuariosRender;
+}
+
+function enviarDatos() {
+    const datos = {
+        nombre: 'Mariano user'
+    }
+    fetch('http://bootcamp-dia-3.camilomontoyau.now.sh/usuarios', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(datos),
+    })
+        .then(response => {
+            response.json()
+                .then(jsonResponse => {
+                    console.log('jsonResponse', jsonResponse)
+                    refrescar();
+                });
+        })
+
+}
+
+function refrescar() {
+    fetch('http://bootcamp-dia-3.camilomontoyau.now.sh/usuarios', {
+        method: 'GET'
+    })
+        .then(response => response.json()
+            .then(respuestaUsuarios => {
+                console.log('respuestaUsuarios',respuestaUsuarios)
+                usuarios = respuestaUsuarios
+                
+            })
+        );
+}
+
+boton.onclick = enviarDatos;
--- xmlhttprequest.js
+++ xmlhttprequest.js
@@ -1,8 +1,30 @@
-function reqListener () {
-    console.log(JSON.parse(this.responseText));
-  }
-  
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", reqListener);
-  oReq.open("GET", "http://www.example.org/example.txt");
-  oReq.send();
+//Document es la API que el navegador me da para jugar con el html de la página
+const listaUsuarios = document.getElementById('lista-usuarios');
+const boton = document.getElementById('boton');
+
+function reqListener() {
+  const usuarios = JSON.parse(this.responseText);
+  console.log(usuarios)
+  const usuariosRender = usuarios.map(usuario => `<li>${usuario.nombre}</li>`);
+  console.log(usuariosRender);
+  listaUsuarios.innerHTML = usuariosRender.join("");
+}
+
+var peticion = new XMLHttpRequest();
+peticion.open("GET", "http://bootcamp-dia-3.camilomontoyau.now.sh/usuarios");
+peticion.send();
+peticion.addEventListener("load", reqListener);
+
+function enviarDatos() {
+  peticion.open("POST", "http://bootcamp-dia-3.camilomontoyau.now.sh/usuarios", true);
+  peticion.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  peticion.send("nombre=LUNES24");
+  setTimeout(refrescar, 3000);
+}
+
+function refrescar() {
+  peticion.open("GET", "http://bootcamp-dia-3.camilomontoyau.now.sh/usuarios");
+  peticion.send();
+}
+
+boton.onclick = enviarDatos;
